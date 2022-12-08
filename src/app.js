7f956a951ad3742b666a815b71768f885de25a15
JoaoVitorRP <joaovrp.mec.ufsc@gmail.com>
2022-12-07 21:21:11 -0300

    feat: get and post categories

--- src/app.js
+++ src/app.js
@@ -1,25 +1,11 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import pkg from "pg";
-
-dotenv.config();
+import categoriesRouter from "./routes/categoriesRoutes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-const { Pool } = pkg;
-
-try {
-  const connection = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  });
-
-  console.log("Connected to Postgres");
-} catch (err) {
-  console.log(err);
-}
+app.use(categoriesRouter);
 
 const port = process.env.PORT || 4000;
 
---
+++ src/controllers/categoriesController.js
@@ -0,0 +1,30 @@
+import { connection } from "../database/db.js";
+import { categorySchema } from "../schemas/categorySchema.js";
+
+export async function getCategories(req, res) {
+  try {
+    const categories = await connection.query("SELECT * FROM categories");
+    res.send(categories.rows);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+}
+
+export async function postCategory(req, res) {
+  const { name } = req.body;
+
+  const { error } = categorySchema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  try {
+    const categoryAlreadyExists = await connection.query(`SELECT * FROM categories WHERE name = '${name}'`);
+
+    if (categoryAlreadyExists.rows.length !== 0) return res.sendStatus(409);
+
+    await connection.query(`INSERT INTO categories (name) VALUES ('${name}')`);
+
+    return res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+}
