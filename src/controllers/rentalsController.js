6c984f46e06a1290fbafea3df137eb089c1d0566
JoaoVitorRP <joaovrp.mec.ufsc@gmail.com>
2022-12-09 19:24:51 -0300

    feat: get rentals

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -1,7 +1,65 @@
 import dayjs from "dayjs";
 import { connection } from "../database/db.js";
 
-export async function getRentals(req, res) {}
+export async function getRentals(req, res) {
+  const { customerId, gameId } = req.query;
+
+  let id;
+  if (customerId) id = customerId;
+  if (gameId) id = gameId;
+
+  try {
+    const rentalsCustomerGame = await connection.query(
+      `
+      SELECT
+        json_agg(
+          json_build_object(
+            'id', r.id,
+            'customerId', r."customerId",
+            'gameId', r."gameId",
+            'rentDate', r."rentDate",
+            'daysRented', r."daysRented",
+            'returnDate', r."returnDate",
+            'originalPrice', r."originalPrice",
+            'delayFee', r."delayFee",
+            'customer', json_build_object(
+              'id', c.id,
+              'name', c.name
+            ),
+            'game', json_build_object(
+              'id', g.id,
+              'name', g.name,
+              'categoryId', g."categoryId",
+              'categoryName', ca.name
+            )
+          )
+        )
+      FROM
+        rentals r
+      JOIN
+        customers c
+      ON
+        r."customerId" = c.id
+      JOIN
+        games g
+      ON 
+        r."gameId" = g.id
+      JOIN
+        categories ca
+      ON
+        g."categoryId" = ca.id
+      ${customerId ? `WHERE "customerId" = $1` : ` `}
+      ${gameId ? `WHERE "gameId" = $1` : ` `};
+    `,
+      [id]
+    );
+
+    res.send(rentalsCustomerGame.rows[0].json_agg);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+}
 
 export async function postRental(req, res) {
   const { customerId, gameId, daysRented } = req.body;
@@ -30,7 +88,7 @@ export async function deleteRental(req, res) {
 
   try {
     await connection.query(`DELETE FROM rentals WHERE id = $1;`, [id]);
-    
+
     res.sendStatus(200);
   } catch (err) {
     res.status(500).send(err);
--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { deleteRental, postRental } from "../controllers/rentalsController.js";
+import { deleteRental, getRentals, postRental } from "../controllers/rentalsController.js";
 import { rentalValidation } from "../middlewares/rentalValidationMiddleware.js";
 import { rentalDeleteValidation } from "../middlewares/rentalDeleteValidationMiddleware.js";
 
 const router = Router();
 
-router.get("/rentals");
+router.get("/rentals", getRentals);
 router.post("/rentals", rentalValidation, postRental);
 router.post("/rentals/:id/return");
 router.delete("/rentals/:id", rentalDeleteValidation, deleteRental);
