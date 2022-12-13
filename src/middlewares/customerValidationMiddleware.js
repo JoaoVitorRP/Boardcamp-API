9a3bb8c3f2a2ec0f77d99fcd4e16aac37bd53145
JoaoVitorRP <joaovrp.mec.ufsc@gmail.com>
2022-12-13 12:56:48 -0300

    fix: editCostumer not working if CPF not changed

--- src/middlewares/customerValidationMiddleware.js
+++ src/middlewares/customerValidationMiddleware.js
@@ -2,13 +2,23 @@ import { connection } from "../database/db.js";
 import { customerSchema } from "../schemas/customerSchema.js";
 
 export async function customerValidation(req, res, next) {
+  const { id } = req.params;
   const { cpf } = req.body;
 
   const { error } = customerSchema.validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   try {
-    const customerWithSameCPF = await connection.query(`SELECT * FROM customers WHERE cpf = '${cpf}'`);
+    const customerWithSameCPF = await connection.query(
+      `
+      SELECT *
+      FROM
+        customers
+      WHERE
+        cpf = '${cpf}'
+      ${id ? `AND id <> ${id}` : ` `};
+      `
+    );
     if (customerWithSameCPF.rows.length !== 0) return res.sendStatus(409);
   } catch (err) {
     return res.status(500).send(err);
